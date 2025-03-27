6c8473bbbcb627bc0bc202b5b6d0cd4399e91e60
LisaTarn <lisatarnowpt@gmail.com>
2025-03-27 09:55:52 -0400

    Added logic for hamburger menu to NavBar.js

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,14 +1,23 @@
 'use client';
+import { useState } from "react";
 import Link from "next/link";
 
 export default function NavBar(){
+    const[isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    } 
     return(
     <nav>
+        <div onClick={toggleMenu}>
         <Link href="/">Home</Link>
         <Link href="/activities">Activities</Link>
         <Link href="/progress">Progress</Link>
         <Link href="/account">Account</Link>
         <Link href="/register">Register</Link>
+        </div>
+        
     </nav>
     )
 }
