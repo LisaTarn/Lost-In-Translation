574db16af4497709a14549652e12745bf71a2550
Anupa <anupa@macbookpro.home>
2025-04-13 23:20:21 -0400

    Fixed logo on homepage

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           <p className={Styles.slogan}>"Learn Languages, Stay L.I.T</p>
           <Link href={`/register`}><button className={Styles.register}>Start Learning</button></Link>
         </div>
-        <Image className={Styles.logo} src="/images/logo.png" alt="Lost in Translation logo" width={600} height={600}/>
+        <Image className={Styles.logo} src="https://lisatarn.github.io/Lost-In-Translation/images/logo.png" alt="Lost in Translation logo" width={600} height={600}/>
       </div>
     </div>
   );
