206a84407f557aa6c980c56458b92e939695b03e
LisaTarn <lisatarnowpt@gmail.com>
2025-04-17 12:44:49 -0400

    Added lazy loading to progress page for ProgressBar component

--- app/progress/page.js
+++ app/progress/page.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ProgressBar from "../components/ProgressBar";
+import React, { Suspense, lazy} from 'react';
+
+const ProgressBar = lazy (() => import ('../components/ProgressBar'));
 
 export default function progress()
 {
     return(
         <div>
+             <Suspense fallback={<p>Loading ...</p>}>
         <ProgressBar />
+        </Suspense>
         </div>
     )
 }
